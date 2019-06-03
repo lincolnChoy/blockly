7a48e2d5e2c6b3b43ebcb63e8ae256553fe23780
Yian Qian <yqia375@aucklanduni.ac.nz>
2019-06-03 16:13:11 +1200

    fixing the formatting of code output

--- demos/code/code.js
+++ demos/code/code.js
@@ -296,52 +296,21 @@ Code.tabClick = function(clickedName) {
   Blockly.fireUiEvent(window, 'resize');
 };
 
+
+
+
 /**
  * Populate the currently selected pane with content generated from the blocks.
  */
-Code.renderContent = function() {
-  var content = document.getElementById('content_' + Code.selected);
-  // Initialize the pane.
-  if (content.id == 'content_xml') {
-    var xmlTextarea = document.getElementById('content_xml');
-    var xmlDom = Blockly.Xml.workspaceToDom(Code.workspace);
-    var xmlText = Blockly.Xml.domToPrettyText(xmlDom);
-    xmlTextarea.value = xmlText;
-    xmlTextarea.focus();
-  } else if (content.id == 'content_javascript') {
-    var code = Blockly.JavaScript.workspaceToCode(Code.workspace);
-    content.textContent = code;
-    if (typeof prettyPrintOne == 'function') {
-      code = content.innerHTML;
-      code = prettyPrintOne(code, 'js');
-      content.innerHTML = code;
-    }
-  } else if (content.id == 'content_python') {
-    code = Blockly.Python.workspaceToCode(Code.workspace);
-    content.textContent = code;
-    if (typeof prettyPrintOne == 'function') {
-      code = content.innerHTML;
-      code = prettyPrintOne(code, 'py');
-      content.innerHTML = code;
-    }
-  } else if (content.id == 'content_dart') {
-    code = Blockly.Dart.workspaceToCode(Code.workspace);
-    content.textContent = code;
-    if (typeof prettyPrintOne == 'function') {
-      code = content.innerHTML;
-      code = prettyPrintOne(code, 'dart');
-      content.innerHTML = code;
-    }
-  } else if (content.id == 'content_java') {
-    code = Blockly.Java.workspaceToCode(Code.workspace);
+renderContent = function() {
+  var content = document.getElementById('content_java');
+    code = Blockly.Java.workspaceToCode(workspace1);
     content.textContent = code;
     if (typeof prettyPrintOne == 'function') {
       code = content.innerHTML;
       code = prettyPrintOne(code, 'java');
-      content.innerHTML = code;
-    }
+      content_java.innerHTML = code;
   }
-  
 };
 
 /**
